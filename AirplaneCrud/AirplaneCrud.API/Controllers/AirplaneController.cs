6edc0c37c5d1ca54793ccef3c44852c09effcec4
Daniel <Daniel@DESKTOP-NTL6TLC>
2019-04-04 19:36:29 -0300

    API

---
+++ AirplaneCrud/AirplaneCrud.API/Controllers/AirplaneController.cs
@@ -0,0 +1,49 @@
+﻿using AirplaneCrud.API.Models;
+using AirplaneCrud.Domain;
+using AirplaneCrud.Domain.Models.Airplane;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AirplaneCrud.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirplaneController : ControllerBase
+    {
+        private readonly IAirplaneDomain airplaneDomain;
+
+        public AirplaneController(IAirplaneDomain airplaneDomain)
+        {
+            this.airplaneDomain = airplaneDomain;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public Task<IEnumerable<IAirplane>> Get([FromQuery] int limit = 20, int offset = 0)
+        {
+            return airplaneDomain.List(limit, offset);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Task<IAirplane> PostAsync([FromBody] CreateAirplane value)
+        {
+            return airplaneDomain.Create(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public Task<IAirplane> Put(string id, [FromBody] CreateAirplane value)
+        {
+            return airplaneDomain.Edit(id, value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public Task Delete(string id)
+        {
+            return airplaneDomain.Remove(id);
+        }
+    }
+}
---
+++ AirplaneCrud/AirplaneCrud.API/Models/CreateAirplane.cs
@@ -0,0 +1,16 @@
+﻿using AirplaneCrud.Domain.Models.Airplane;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirplaneCrud.API.Models
+{
+    public class CreateAirplane : ICreateAirplane
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Model { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue)]
+        public int MaxPassengers { get; set; }
+    }
+}
