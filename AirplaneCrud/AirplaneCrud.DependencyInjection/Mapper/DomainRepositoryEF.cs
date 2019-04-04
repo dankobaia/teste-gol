5ef5bd28aa444f1030bab5e3ebc8a3b109f80755
Daniel <Daniel@DESKTOP-NTL6TLC>
2019-04-04 19:36:16 -0300

    DI

---
+++ AirplaneCrud/AirplaneCrud.DependencyInjection/Mapper/DomainRepositoryEF.cs
@@ -0,0 +1,26 @@
+﻿namespace AirplaneCrud.DependencyInjection.Mapper
+{
+    internal class DomainRepositoryEF : AutoMapper.Profile
+    {
+        public DomainRepositoryEF()
+        {
+            AutoMapperConfig();
+            MapRepositoryModels();
+        }
+
+        private void AutoMapperConfig()
+        {
+            // map properties with a public or private getter
+            ShouldMapProperty = pi =>
+                pi.GetMethod != null && (pi.GetMethod.IsPublic || pi.GetMethod.IsPrivate);
+        }
+
+        private void MapRepositoryModels()
+        {
+            CreateMap<Repository.EF.Models.Airplane, Domain.Models.Airplane.IAirplane>()
+                .As<Domain.Gol.Models.Airplane>();
+
+            CreateMap<Repository.Models.Airplane.IAirplaneRepositoryModel, Domain.Gol.Models.Airplane>();
+        }
+    }
+}
---
+++ AirplaneCrud/AirplaneCrud.DependencyInjection/Services/ApiServices.cs
@@ -0,0 +1,23 @@
+﻿using AirplaneCrud.DependencyInjection.Mapper;
+using AirplaneCrud.Domain;
+using AirplaneCrud.Repository;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AirplaneCrud.DependencyInjection.Service
+{
+    public static class ApiServices
+    {
+        public static void ResolveApi(this IServiceCollection services)
+        {
+            services.AddScoped<IAirplaneRepository, AirplaneCrud.Repository.EF.AirplaneRepository>();
+
+            services.AddScoped<IAirplaneDomain, AirplaneCrud.Domain.Gol.AirplaneDomain>();
+
+            var mappingConfig = new MapperConfiguration(mc => mc.AddProfile(new DomainRepositoryEF()));
+            mappingConfig.AssertConfigurationIsValid();
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+    }
+}
